b89b020ca2d6f36d4189c6b44b980c3241e3b661
Cary Bondoc <cbondoc.it@gmail.com>
2022-09-06 20:24:21 +0800

    unknown commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Redirect, Response, File;
 use Illuminate\Support\Facades\Hash;
+use Redirect, Response, File;
 
 class UserController extends Controller
 {
